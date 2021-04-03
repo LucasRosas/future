731012b9ecb5e4955de9dd342ed65659a481883f
LucasRosas <lucasrosas@outlook.com>
2021-04-02 23:14:55 -0300

    main

--- js/future.js
+++ js/future.js
@@ -1,6 +1,25 @@
 var entrada
+var texto = 'memoria'
+var prefixo = `<section class='completo'>`
+var posfixo = '</section>'
+var etapa = 'etapa'
+var efeitos = true
 
 
+function efeito(x) {
+    if (efeitos) {
+        efeitos = false
+        x.textContent = 'desativar efeitos'
+        localStorage.setItem('etapa', 0)
+        mudaestilo('dist', '100px')
+        roda()
+    } else {
+        efeitos = true;
+        x.textContent = 'ativar efeitos'
+        mudaestilo('dist', '0px')
+    }
+    console.log(efeitos);
+}
 
 function cornome(x) {
     x.nextElementSibling.value = x.value
@@ -80,53 +99,16 @@ function marcaiframe() {
 
 }
 
-var texto = 'memoria'
-var prefixo = `<section class='completo'>`
-var posfixo = '</section>'
-
-function roda() {
-    entrada = document.getElementById('blocodenotas').innerHTML.replaceAll('<span style="text-indent: 1.5em;">', '').replaceAll('</span>', '')
-    localStorage.setItem(texto, entrada)
-    entrada = entrada.replaceAll('div>', 'p>').replaceAll('<p>iframe_', '<p class="iframe">iframe_').replaceAll('<p>_</p>', `<button class='botaocontinua' onclick="continua(this)">Continuar</button></section><section style="display:none">`)
-    let resp = []
-    entrada = complete(entrada)
-    let code = []
-    entrada = iframe(entrada)
-
-    entrada = tag(entrada, '###', '<h3>', '</h3>')
-    entrada = tag(entrada, '##', '<h2>', '</h2>')
-    entrada = tag(entrada, '#', '<h1>', '</h1>')
-    entrada = txt(entrada, '<p>-#</p>', '<ol>', '</ol>')
-    entrada = tag(entrada, '-', '<li>', '</li>')
-
-    entrada = tag(entrada, 'img',
-        '<img src="', '">')
-    entrada = txt(entrada, '***', '<u>', '</u>')
-    entrada = txt(entrada, '**', '<i>', '</i>')
-    entrada = txt(entrada, '*', '<strong>', '</strong>')
-    entrada = link(entrada)
-    entrada = tabela(entrada)
-    entrada = imagem(entrada)
-
-
-    document.getElementById('preview').innerHTML = prefixo + entrada + posfixo
-    chamaiframe()
-    renderMathInElement(document.getElementById('preview'), {
-        delimiters: [{
-            left: "$$",
-            right: "$$",
-            display: true
-        }, { left: "\\[", right: "\\]", display: true }, { left: "$", right: "$", display: false }, { left: "\\(", right: "\\)", display: false }]
-    })
-
-}
-
-
 function continua(x) {
     confere(x)
     if (x.parentElement.classList.contains('completo')) {
         x.parentElement.nextElementSibling.style.display = 'block'
         x.style.display = 'none'
+        k = Array.from(document.getElementsByClassName('botaocontinua')).indexOf(x) + 1
+        console.log(k);
+        if (localStorage.getItem('etapa') < k) {
+            localStorage.setItem('etapa', k)
+        }
     } else {
         alert('Complete todas as Quests para continuar')
     }
@@ -263,5 +245,55 @@ roda()
 
 function mudaestilo(a, b) {
     document.documentElement.style.setProperty('--' + a, b);
+
+}
+
+function abre(n) {
+    console.log(n);
+    for (i = 0; i <= n; i++) {
+        document.getElementsByClassName('botaocontinua')[i].style.display = 'none'
+        document.getElementsByTagName('section')[i].style.display = 'block'
+    }
+    document.getElementsByClassName('botaocontinua')[n].style.display = 'inline'
+}
+
+
+function roda() {
+    entrada = document.getElementById('blocodenotas').innerHTML.replaceAll('<span style="text-indent: 1.5em;">', '').replaceAll('</span>', '')
+    localStorage.setItem(texto, entrada)
+    entrada = entrada.replaceAll('div>', 'p>').replaceAll('<p>iframe_', '<p class="iframe">iframe_').replaceAll('<p>_</p>', `<button class='botaocontinua' onclick="continua(this)">Continuar</button></section><section style="display:none">`)
+    let resp = []
+    entrada = complete(entrada)
+    let code = []
+    entrada = iframe(entrada)
+
+    entrada = tag(entrada, '###', '<h3>', '</h3>')
+    entrada = tag(entrada, '##', '<h2>', '</h2>')
+    entrada = tag(entrada, '#', '<h1>', '</h1>')
+    entrada = tag(entrada, '      ', `<p style="text-align: right; text-indent:0px">`, '</p>')
+    entrada = tag(entrada, '   ', `<p style="text-align: center; text-indent:0px">`, '</p>')
+    entrada = txt(entrada, '<p>-#</p>', '<ol>', '</ol>')
+    entrada = tag(entrada, '-', '<li>', '</li>')
+
+    entrada = tag(entrada, 'img',
+        '<img src="', '">')
+    entrada = txt(entrada, '***', '<u>', '</u>')
+    entrada = txt(entrada, '**', '<i>', '</i>')
+    entrada = txt(entrada, '*', '<strong>', '</strong>')
+    entrada = link(entrada)
+    entrada = tabela(entrada)
+    entrada = imagem(entrada)
+
+
+    document.getElementById('preview').innerHTML = prefixo + entrada + posfixo
+    chamaiframe()
+    abre(localStorage.getItem('etapa'))
+    renderMathInElement(document.getElementById('preview'), {
+        delimiters: [{
+            left: "$$",
+            right: "$$",
+            display: true
+        }, { left: "\\[", right: "\\]", display: true }, { left: "$", right: "$", display: false }, { left: "\\(", right: "\\)", display: false }]
+    })
 
 }
